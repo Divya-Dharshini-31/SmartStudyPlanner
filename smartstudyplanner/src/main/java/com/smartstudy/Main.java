f94ab481c4b0978fba7e3663ca0b5ff61a65d50f
Divya-Dharshini-31 <sdivyadharshini315@gmail.com>
2025-12-09 22:12:16 +0530

    some updates

---
+++ smartstudyplanner/src/main/java/com/smartstudy/Main.java
@@ -0,0 +1,25 @@
+package com.smartstudy;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello Smart Study Planner!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 600, 400);
+
+        primaryStage.setTitle("Smart Study Planner");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
